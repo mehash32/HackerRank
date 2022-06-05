c2426e4740c2738ec3912318ed64a572e5e63f2d
mehash32 <65710141+mehash32@users.noreply.github.com>
2022-06-06 02:31:17 +0600

    Add files via upload

---
+++ day1.py
@@ -0,0 +1,15 @@
+from numpy import double
+
+
+n=int(input().strip())
+a= double(input().strip())
+str= input()
+i=4
+d=4
+sum=0
+sum=n+i
+doubleSum= a+d
+outputStr="Hacker Rank " + str
+print(int(sum))
+print(doubleSum)
+print(outputStr)
